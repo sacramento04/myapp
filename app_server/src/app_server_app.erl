c05a902991edddd4d9efecd635bd7aeabefef819
sacramento04 <sacramento04@163.com>
2015-08-28 19:23:47 +0800

    add code

--- app_server/src/app_server_app.erl
+++ app_server/src/app_server_app.erl
@@ -1,16 +1,76 @@
 -module(app_server_app).
 
 -behaviour(application).
+-behaviour(supervisor).
 
-%% Application callbacks
--export([start/2, stop/1]).
+-export([
+    start/0,
+    start/2,
+    stop/1
+]).
 
-%% ===================================================================
-%% Application callbacks
-%% ===================================================================
+-export([
+    init/1
+]).
 
-start(_StartType, _StartArgs) ->
-    app_server_sup:start_link().
+-include("app_server_app.hrl").
 
-stop(_State) ->
+
+start () ->
+    application:start(?MODULE).
+    
+start (_Type, _StartArgs) ->
+    file:make_dir(?LOG_DIR),
+    inets:start(),
+    Result = supervisor:start_link({local, ?MODULE}, ?MODULE, []),
+    
+    start_game_app_log(),
+    start_socket_proxy_sup(),
+    start_socket_sup(),
+    
+    Result.
+    
+stop (_State) ->
     ok.
+    
+init ([]) ->
+    {ok, {{one_for_one, 0, 1}, []}}.
+    
+start_game_app_log () ->
+    {ok, _} = supervisor:start_child(
+        ?MODULE, 
+        {
+            app_server_log, 
+            {app_server_log, start_link, []}, 
+            permanent, 
+            16#FFFFFFFF, 
+            worker, 
+            [app_server_log]
+        }
+    ).
+    
+start_socket_proxy_sup () ->
+    {ok, _} = supervisor:start_child(
+        ?MODULE,
+        {
+            socket_proxy_sup,
+            {socket_proxy_sup, start_link, []},
+            permanent,
+            16#FFFFFFFF,
+            supervisor,
+            [socket_proxy_sup]
+        }
+    ).
+    
+start_socket_sup () ->
+    {ok, _} = supervisor:start_child(
+        ?MODULE,
+        {
+            socket_sup,
+            {socket_sup, start_link, []},
+            permanent,
+            16#FFFFFFFF,
+            supervisor,
+            [socket_sup]
+        }
+    ).
